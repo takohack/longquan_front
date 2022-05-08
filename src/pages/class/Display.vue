4361cabe3578e86455e28758cfccf03dd8d04db1
takohack <1349607493@qq.com>
2022-05-08 22:33:22 +0800

    5.8

--- src/pages/class/Display.vue
+++ src/pages/class/Display.vue
@@ -36,7 +36,7 @@
                     rounded
                     small
                     @click="routerTo(item)"
-                    >订阅</v-btn
+                    >{{ item.type }}</v-btn
                   >
                 </v-card-actions>
               </div>
@@ -54,6 +54,12 @@
 <script>
 import axios from "axios";
 export default {
+  props: ["choose_Lesson"],
+  watch: {
+    choose_Lesson: function (val) {
+      console.log(val); // 接收父组件的值
+    },
+  },
   components: {},
   data() {
     return {
@@ -65,22 +71,51 @@ export default {
     this.request();
   },
   computed: {
-    items() {
-      let items = [];
-      for (let i = 0, len = this.courses.length; i < len; i++) {
-        let a = {};
-        a.image = this.courses[i].fields.courseImg;
-        a.title = this.courses[i].fields.courseName;
-        a.category =
-          this.courses[i].fields.teacherName +
-          " 开课学期:" +
-          this.courses[i].fields.courseTerm;
-        a.keyword = "理学院";
-        console.log(a);
-        items.push(a);
-      }
-      console.log(items);
-      return items;
+    course_id: function () {
+      let map = new Map();
+      map.set("数据结构", 3);
+      map.set("数学思想与数模文化", 1);
+      map.set("多元统计与数学分析", 2);
+      map.set("高等代数", 4);
+      return map;
+    },
+    items: {
+      get() {
+        let items = [];
+        let course_id = this.choose_Lesson.split(",");
+        for (let i = 0, len = this.courses.length; i < len; i++) {
+          let a = {};
+          a.image = this.courses[i].fields.courseImg;
+          a.title = this.courses[i].fields.courseName;
+          a.category =
+            this.courses[i].fields.teacherName +
+            " 开课学期:" +
+            this.courses[i].fields.courseTerm;
+          a.keyword = "理学院";
+          a.type = "订阅";
+          if (course_id.includes(this.courses[i].fields.courseId)) {
+            a.type = "取消订阅";
+          }
+          console.log(a);
+          items.push(a);
+        }
+        return items;
+      },
+      set(newvalue) {
+        for (let i = 0, len = this.items.length; i < len; i++) {
+          let a = this.items[i];
+          if (a.title === newvalue) {
+            if (a.type === "订阅") {
+              a.type = "取消订阅";
+            } else {
+              a.type = "订阅";
+            }
+            console.log(a.type);
+            console.log(this.searching);
+          }
+        }
+        this.$forceUpdate();
+      },
     },
     keywords() {
       if (!this.search) return [];
@@ -120,9 +155,51 @@ export default {
           console.log(error);
         });
     },
+    addcourse(item) {
+      console.log("订阅课程" + item);
+      let course_id = this.course_id.get(item);
+      axios
+        .get(`http://localhost:8080/user/addcourse?course_id=${course_id}`)
+        .then(
+          (response) => {
+            if (response.data.result === "success") {
+              console.log("修改computed属性" + item);
+              this.items = item;
+              console.log(this.items);
+            }
+          },
+          (error) => {
+            console.log("请求失败", error.message);
+          }
+        );
+      console.log(course_id);
+    },
+    delcourse(item) {
+      console.log("取消订阅课程" + item);
+      let course_id = this.course_id.get(item);
+      axios
+        .get(`http://localhost:8080/user/delcourse?course_id=${course_id}`)
+        .then(
+          (response) => {
+            if (response.data.result === "success") {
+              console.log("修改computed属性" + item);
+              this.items = item;
+              console.log(this.items);
+            }
+          },
+          (error) => {
+            console.log("请求失败", error.message);
+          }
+        );
+      console.log(course_id);
+    },
     routerTo(item) {
-      let course_name = item.fields.courseName;
-      this.$router.push({ path: "/course", query: { course: course_name } });
+      let type = item.type;
+      if (type === "订阅") {
+        this.addcourse(item.title);
+      } else {
+        this.delcourse(item.title);
+      }
     },
   },
 };
